9862a800f7fc445cfb82ba2343344c4b8a45dc65
Vencislav <vencislav67@hotmail.com>
2022-06-21 16:20:41 +0300

    5

---
+++ 05_Applied_Arithmetics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05_Applied_Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string cmnd;
+
+            while((cmnd = Console.ReadLine()) != "end")
+            {
+
+            }
+        }
+    }
+}
