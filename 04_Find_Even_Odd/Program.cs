2e2ad1c8dca529b9542af5370be1f9e6018fec83
Vencislav <vencislav67@hotmail.com>
2022-06-21 16:11:25 +0300

    4

---
+++ 04_Find_Even_Odd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_Find_Even_Odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string range = Console.ReadLine();
+            int startRange = int.Parse(range.Split()[0]);
+            int endRange = int.Parse(range.Split()[0]);
+            string cmnd = Console.ReadLine();
+
+            List<int> numbers = new List<int>();
+
+            for(int i = startRange; i <= endRange; i++)
+            {
+
+            }
+        }
+    }
+}
