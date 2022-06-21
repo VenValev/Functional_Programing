e1b118e841fc7d6c7b56931440e8973f446dfd26
Vencislav <vencislav67@hotmail.com>
2022-06-21 16:30:55 +0300

    6

---
+++ 06_Reverse_And_Exclude/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06_Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+        }
+    }
+}
