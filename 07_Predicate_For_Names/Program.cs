94f73bd0e971480cc1013a7d948ae71d249b1127
Vencislav <vencislav67@hotmail.com>
2022-06-21 16:43:38 +0300

    7

--- 07_Predicate_For_Names/Program.cs
+++ 07_Predicate_For_Names/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _07_Predicate_For_Names
 {
@@ -6,7 +8,12 @@ namespace _07_Predicate_For_Names
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            //List<string> namesList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Predicate<string> exclude = name => name.Length <= n;
+
+            Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList().FindAll(exclude).ForEach(Console.WriteLine);
         }
     }
 }
