85f103af01a29f73db79d901ea424976e6c0fd2a
Vencislav <vencislav67@hotmail.com>
2022-06-21 15:39:24 +0300

    2

---
+++ 02_Knights_Of_Honor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Knights_Of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> namesList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+        }
+    }
+}
