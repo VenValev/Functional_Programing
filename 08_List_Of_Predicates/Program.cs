52d34e4ea834cc30697975f069fb82c935318147
Vencislav <vencislav67@hotmail.com>
2022-06-21 17:16:36 +0300

    8

--- 08_List_Of_Predicates/Program.cs
+++ 08_List_Of_Predicates/Program.cs
@@ -9,18 +9,45 @@ namespace _08_List_Of_Predicates
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            List<int> deviders = Console.ReadLine()
-                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToList();
-            List<int> numbers = new List<int>();
+            List<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToList();
 
-            for (int i = 0; i < n; i++)
+
+            List<int> numbers = new List<int>(); //всички числа от 1 до n
+            for (int number = 1; number <= n; number++)
             {
-                numbers.Add(i);
+                numbers.Add(number);
             }
 
+            List<int> printNumbers = new List<int>(); //числата, които се делят на всички делители
 
+            foreach (int number in numbers)
+            {
+                bool isDivisible = true;
+                //true -> ако числото се дели на всички делители
+                //false -> ако числото не се дели на всички делители
+
+                //ако се дели на всички числа без остатък от dividers
+                foreach (int divider in dividers)
+                {
+                    //приема число -> дали числото се дели на divider
+                    Predicate<int> divisible = number => number % divider == 0;
+                    //true -> числото се дели на текущия делител
+                    //false -> числото не се дели на текущия делител
+                    if (!divisible(number)) //number % divider == 0
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    printNumbers.Add(number);
+
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", printNumbers));
         }
     }
 }
