094eec9113b27aac4b931ab7c9c11b8b19c2e70d
Vencislav <vencislav67@hotmail.com>
2022-06-21 16:48:11 +0300

    8

---
+++ 08_List_Of_Predicates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08_List_Of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<int> deviders = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(i);
+            }
+        }
+    }
+}
