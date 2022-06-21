1b1666ff65c6b1976321ddbe57aba58ace15bfec
Vencislav <vencislav67@hotmail.com>
2022-06-21 16:37:42 +0300

    7

---
+++ 07_Predicate_For_Names/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07_Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
