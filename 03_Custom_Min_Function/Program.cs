c8bc5a3f7df37e4519730f6ecba8247db913eb68
Vencislav <vencislav67@hotmail.com>
2022-06-21 15:52:14 +0300

    3

---
+++ 03_Custom_Min_Function/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03_Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            Func<List<int>, int> minFunc = numbers => numbers.Min();
+
+            Console.WriteLine(minFunc(numbersList));
+        }
+    }
+}
