3d837caaeb0dba4e6201bcb8ac0e9b5ab8a34734
Vencislav <vencislav67@hotmail.com>
2022-06-21 17:24:57 +0300

    11

---
+++ 11_TriFunction/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _11_TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int treshold = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ");
+            Console.WriteLine(names.First(name => name.Select(symbol => (int)symbol).Sum() >= treshold));
+        }
+    }
+}
